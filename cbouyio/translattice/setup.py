256282355aa125be6e72ceca6102571a5025067b
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2006-09-18 17:58:26 +0000

    Introduce the translattice directory. First upload of the setup.py script. Some minor fixes in the modules docstrings.

---
+++ cbouyio/translattice/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='translattice',
+  version='0.1.0',
+  description='A python module for lattice simulation of transsys programs',
+  long_description='pending...',
+  py_modules=['translattice'],
+  author='Costas Bouyioukos',
+  author_email='konsb@cmp.uea.ac.uk',
+  )
+
