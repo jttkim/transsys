dbbb9b6daef7e5b1db7b83b2573f10bd9c0c7704
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2006-10-16 16:44:27 +0000

    The script take one argument (the transsys program filename). Also exits with a non zero exit code if something goes wrong.

--- cbouyio/analyseLattice.sh
+++ cbouyio/analyseLattice.sh
@@ -3,35 +3,77 @@
 # Shell script to wrap all the lattice simulation procedure.
 
 # Usage.
-#if [ "$1" == "--help" ] || [ "$1" = "-h" ] ;
+if test "$1" == "--help"  || test "$1" == "-h" ;
+then
+  echo "Usage: $0 transsys_program"
+  echo "This is the minimum requirement for the script to run.
+For more info about the script check the source code."
+  exit 1
+fi
+
+if test $# -lt 1 ;
+then
+  echo "$0 : a transsys program file is required to run the script."
+  exit 1
+fi
+
+tpName=$1;
+
+if ! test -e "$tpName" ;
+then
+  echo "Input file $tpName does not exist"
+  exit 
+fi
+
+bname=${tpName%\.tra}
+
+#if test $bname -eq 1 ;
 #then
-#  echo "Usage: $0 transsys_program lattice_size timesteps"
-#  echo "This is the minimum requirement for the script to run.
-#For more info about the script check the source code."
+#  echo "Give a transsys program file .tra"
 #  exit 1
 #fi
 
-#if (( $# < 3 )) ;
-#then
-#  echo "$0 : The script works with at least 3 parameters check help."
-#  exit 1
-#fi
+#echo $bname
+
+#bname=`basename $1`
+
+#echo $bname
+
+#exit 1
 
 
 # Start the process.
-./latticeSimulator -n 10x10 -t 100 -u 0:1 patternFormation.tra patternFormation_ftable.dat
+if ! ./latticeSimulator -n 10x10 -t 10 -u 0:1 $tpName ${bname}_ftable.dat ;
+then
+  exit $?
+fi
 
 # Invoke R (applies only to the patternFormation.tra transsys program)
-echo "source("\""translattice.r"\"")" | R CMD BATCH
-echo "lframe <- readTransLattice("\""patternFormation_ftable.dat"\"")" | R CMD BATCH
-echo "postscript("\""patternFormation.ps"\"", width=10, height=10); plotConcentrationSeries(lframe, "\""factor_activator"\"", getConcentrationRange(lframe, "\""factor_activator"\""), oneSecondDelay); dev.off();" | R CMD BATCH
+echo "source("\""translattice.r"\"")" | R CMD BATCH --no-restore
+echo "lframe <- readTransLattice("\""${bname}_ftable.dat"\"")" | R CMD BATCH
+
+#echo "postscript("\""patternFormation.ps"\"", width=10, height=10); plotConcentrationSeries(lframe, "\""factor_activator"\"", getConcentrationRange(lframe, "\""factor_activator"\""), oneSecondDelay); dev.off();" | R CMD BATCH
 
 
-# Run the control.
-./zeroTranssysDiffusibility patternFormation.tra patternFormation_zeroDiff.tra
-./latticeSimulator -n 10x10 -t 100 -u 0:1 patternFormation_zeroDiff.tra patternFormation_zeroDiff_ftable.dat
+# Generate the control.
+./zeroTranssysDiffusibility $tpName ${bname}_zeroDiff.tra
+
+if ! ./zeroTranssysDiffusibility $tpName ${bname}_zeroDiff.tra ;
+then
+  exit $?
+fi
+
+# Run the control
+if ! ./latticeSimulator -n 10x10 -t 10 -u 0:1 ${bname}_zeroDiff.tra ${bname}_zeroDiff_ftable.dat ;
+then
+  exit $?
+fi
 
 # Invoke R.
-echo "lframe_zero <- readTransLattice("\""patternFormation_zeroDiff_ftable.dat"\"")" | R CMD BATCH
-echo "postscript("\""patternFormation_zeroDiff.ps"\"", width=10, height=10); plotConcentrationSeries(lframe_zero, "\""factor_activator"\"", getConcentrationRange(lframe_zero, "\""factor_activator"\""), oneSecondDelay); dev.off();" | R CMD BATCH
+echo "lframe_zero <- readTransLattice("\""${bname}_zeroDiff_ftable.dat"\"")" | R CMD BATCH
+
+#echo "postscript("\""patternFormation_zeroDiff.ps"\"", width=10, height=10); plotConcentrationSeries(lframe_zero, "\""factor_activator"\"", getConcentrationRange(lframe_zero, "\""factor_activator"\""), oneSecondDelay); dev.off();" | R CMD BATCH
+
+# Call an R procedure which contains all the necessary elements.
+R --no-save
 
