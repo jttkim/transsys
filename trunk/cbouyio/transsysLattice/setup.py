f7c591b8c7c474deffb7c69dfa79db12c3afd935
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2006-11-28 01:01:08 +0000

    Some minor style fixes. Implement the __version__ function.

--- trunk/cbouyio/transsysLattice/setup.py
+++ trunk/cbouyio/transsysLattice/setup.py
@@ -8,8 +8,8 @@ from distutils.core import setup
 
 setup(name='translattice',
   version='0.1.1',
-  description='A python module for lattice simulation of transsys programs',
-  long_description='This setup apart from the python module, includes several scripts, installed in the prefix/bin directory. These scripts are essential for conducting any simulation studies and they include: the latticeSimulator script (runs the simulator according to the user specified control parameters), the alterTranssysDiffusibility (produces the two "control" transsys programs) and the conductWholeExperiment.sh (wraps all the scripts together and produce an .R source file ready to load and analyse the data to R) the conductWholeExperiment.sh is obvious that is not a python but a shell script',
+  description='A python module for 2D structure simulation of transsys programs',
+  long_description='The setup apart from the python module translattice.py , installs several scripts, -in the <prefix>/bin directory. These scripts are essential for conducting simulation studies and they include: the latticeSimulator script (runs the simulator according to the user specified control parameters), the alterTranssysDiffusibility (produces the two "control" transsys programs) and the conductWholeExperiment.sh (wraps all the scripts together and produce an .R source file ready to load and analyse the data to R) the conductWholeExperiment.sh is obvious that is not a python but a shell script',
   py_modules=['translattice'],
   scripts=['latticeSimulator', '../scripts/alterTranssysDiffusibility', 'conductWholeExperiment.sh'],
   author='Costas Bouyioukos',
