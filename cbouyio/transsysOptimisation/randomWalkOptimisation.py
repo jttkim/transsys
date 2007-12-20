a8e9ceedd38a9c05457e8c0fcbd06ec170b3bbc4
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2007-12-20 20:55:15 +0000

    Initial upload of the outer shell (design interface and comments) of optimisation experiment sources.

--- cbouyio/transsysOptimisation/randomWalkOptimisation.py
+++ cbouyio/transsysOptimisation/randomWalkOptimisation.py
@@ -5,13 +5,14 @@
 # $Author::            $:  Author of last commit
 # $Date$:  Date of last commit
 
-"""General description here.
+"""Python module containing all the functionalities to conduct the optimisation
+experiments.
 
 @author: Costas Bouyioukos
 @email: konsb@cmp.uea.ac.uk
 @organisation: University of East Anglia
-@since: <<DATE>>
-@license: GNU General Public Lisence 2 or newer.
+@since: 20/12/2007
+@license: GNU General Public Lisence 3 or newer.
 @contact: U{Costas Bouyioukos<mailto:konsb@cmp.uea.ac.uk>}
 @version: $Id$"""
 
@@ -20,60 +21,74 @@
 __version__ = "$Id$"
 
 
-import getopt
-import sys
+import copy
 
-import transsys
-
-
-# Name.
-__name__ = sys.argv[0]
-
-def usage() :
-  print """Usage:"""
-  print __name__, """[options] ARGUMENTS
-Options:
-  -h, --help : Print this screen and exit.
-  -v, --version : Print the program revision information.
-ARGUMENTS"""
+import transsyslattice
+import bimodalities
 
 
-## Command line parsing.
-optlist, args = getopt.getopt(sys.argv[1:], 'hv', ['help', 'version'])
-
-for opt, par in optlist :
-  if opt in ('-h'):
-    usage()
-    sys.exit()
-  if opt in ('--help'):
-    print __name__
-    print __doc__
-    usage()
-    sys.exit()
-  if opt in ('-v', '--version'):
-    print __version__
-    print __doc__
-    sys.exit()
+# Random object already implemeted in the transsysLattice module.
 
 
-# I/O implementation.
-if len(args) > 0 :
-  infile = open(args[0], 'r')
-else :
-  infile = sys.stdin
-if len(args) > 1 :
-  outfile = open(args[1], 'w')
-else :
-  outfile = sys.stdout
+# The perturbed transsys program object.
+
+class PerturbTranssysProgram(transsys.TranssysProgram):
+  """A class to implement the transsys program random walk
+
+  Contains all the necessay methods to perform the transsys program
+  randomisation.
+  """
+
+  def __init__(self, tp, cycle, rndSeed=1);
+    """The constructor
+
+    @param tp: the transsys program.
+    @type tp: C{class 'transsys.TranssysProgram'}
+    @param cycle: the optimisation cycle.
+    @type cycle: C{int}
+    @param rndSeed: the random seed.
+    @type rndSeed: C{int}
+    """
+
+  def randomStep(self, tp, rndSeed):
+    """A function wich perturbs a transsys program by one random step.
+
+    @param tp: the transsys program.
+    @type tp: C{class 'transsys.TranssysProgram'}
+    @param rndSeed: the random seed.
+    @type rndSeed: C{int}
+    """
 
 
-# Main program
+def runTranssysLatticeExperiment(transsysProgram, latticeSize, timesteps,
+    experiment, samplingInterval=timesteps):
+  """A function to run the transsyslattice simulator.
+
+  The function deals with the running of the transsys lattice experiment and
+  returns a transsyslattice.TranssysLatticeTimeseries object
+  @param transsysProgram: A valid transsys program.
+  @type transsysProgram: C{class 'transsys.TranssysProgram'}
+  @param latticeSize: A list denoting the dimensions of the lattice.
+  @type latticeSize: c{list} of C{int}
+  @param timesteps: The number of timesteps for the lattice experiment.
+  @type timesteps: C{int}
+  @param experiment: The type of experiment that we need to conduct, lattice
+  (with difussion) or the control.
+  @type experiment: C{logical}
+  @param samplingInterval: The interval between sampling. It is the same with
+  the number of timesteps because we need to keep only the final state of the
+  simulator.
+  @type samplingInterval: C{int}
+  @return: C{class 'transsyslattice.TranssysLatticeTimeseries'}
+  """
 
 
+def calculateBimodality(transsysLatticeTimeseries):
+  """A function which wraps all the bimodality module functionalities and
+  calculates the total bimodality of a collection of transsys instances.
 
-# Close outfile and exit.
-if outfile is not sys.stdout:
-  outfile.close()
-
-sys.exit
-
+  Returns a scalar.
+  @param transsysLatticeTimeseries: A transsys lattice timeseries contains the
+  transsys lattice mulator's output.
+  @type transsysLatticeTimeseries: C{class 'transsyslattice.TranssysLatticeTimeseries'}
+  """
