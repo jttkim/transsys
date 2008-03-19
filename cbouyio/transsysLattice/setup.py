c6ad590fdcb8bff91a4434376c106aeb602c40e2
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2008-03-19 23:09:53 +0000

    Final place of the optimisation directory (as a transsys lattice subdirectory) and a new setup.py file to install the optimisation modules and the runOptimiser script.

--- cbouyio/transsysLattice/setup.py
+++ cbouyio/transsysLattice/setup.py
@@ -9,9 +9,11 @@ from distutils.core import setup
 setup(name='translattice',
   version='0.1.1',
   description='A python module for 2D structure simulation of transsys programs',
+  #FIXME The discription should be changed after the introduction of the
+  # optimisation modules.
   long_description='The setup apart from the python module translattice.py , installs several scripts, -in the <prefix>/bin directory. These scripts are essential for conducting simulation studies and they include: the latticeSimulator script (run the simulator according to the user specified control parameters) and the alterTranssysDiffusibility (produce the "control" transsys programs). All the experiments can be conducted automatically with the latticeExperinents.sh shell shell script (it is not part of the install).',
-  py_modules=['translattice'],
-  scripts=['latticeSimulator', '../scripts/alterTranssysDiffusibility'],
+  py_modules=['translattice', 'randomLocalSearch', 'bimodalities'],
+  scripts=['latticeSimulator', 'optimisation/runOptimiser', '../scripts/alterTranssysDiffusibility'],
   author='Costas Bouyioukos',
   author_email='konsb@cmp.uea.ac.uk',
   classifiers=[
