6a04ad6c30b65c3ec06fc718f9e09fc37737ed13
cbouyio <cbouyio@86419537-b80a-0410-aa3b-debf91438695>
2008-04-18 21:16:21 +0000

    Minor docstring corrections on the bimodalities and randomLocalSearch modules.
    Adding of some new plotting functions in the .R analysis script.

--- trunk/cbouyio/transsysLattice/optimisation/analyseOptimisation.R
+++ trunk/cbouyio/transsysLattice/optimisation/analyseOptimisation.R
@@ -2,7 +2,22 @@
 # optimisation experiments output.
 
 
-plotObjective <- function(data)
+plotBestObjective <- function(data)
+# Plot the optimisation objective score of r the current best transsys program.
+{
+plot(data$OptCycle, data$BestObj, type= "l", ylim = c(min(data$BestObj), max(data$BestObj)), main = "Current Best's optimisation objective score", xlab = "Optimisation Cycles", ylab = "Optimisation Objective");
+}
+
+
+plotAltObjective <- function(data)
+# Plot the optimisation objective score of r the current alternative transsys
+# program.
+{
+plot(data$OptCycle, data$AltObj, type= "l", ylim = c(min(data$AltObj), max(data$AltObj)), main = "Current Alternative's optimisation objective score", xlab = "Optimisation Cycles", ylab = "Optimisation Objective");
+}
+
+
+plotObjectives <- function(data)
 # Function to plot the Objective Score of the current best transsys program
 # (red) and the objective of the current alternative (blue). The actual
 # optimisation steps (i.e. the cycle where the current alternative is better
@@ -14,12 +29,22 @@ plotObjective <- function(data)
   points(dOpt$OptCycle, (dOpt$OptFlag - abs(min(data$BestObj, data$AltObj))), pch = "|");
 }
 
-extractPostscript <- function(filename, data)
+
+extractPostscript <- function(filename, data, h=8, w=10)
 # Function to produce a .ps of the plot that is the output of the plotObjective
 # function. Takes the filename as an argument.
 {
-  postscript(filename, horizontal = FALSE, paper = "special", height = 8, width = 10, onefile = FALSE);
-  plotObjective(data);
+  postscript(filename, horizontal = FALSE, paper = "special", height = h, width = w, onefile = FALSE);
+  plotObjectives(data);
   dev.off();
+}
+
+
+bimodalitiesTabular <- function(data)
+# Produce a tabular representation of the summary statistics for all the
+# calcuated bimodalities.
+{
+  a <- data.frame(data$AltObj, data$AltLatBM, data$AltCtrlBM, data$BestObj, data$BestLatBM, data$BestCtrlBM);
+  return(summary(a))
 }
 
