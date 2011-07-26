98bdaf2a0fdc587a27ac6020d83fc5d85b482e58
jtk <jtk@86419537-b80a-0410-aa3b-debf91438695>
2011-07-26 12:31:09 +0000

    overhauled / extended facilities for generating dot output

--- trunk/python/setup.py
+++ trunk/python/setup.py
@@ -33,5 +33,5 @@ distutils.core.setup(name = 'transsys',
                      author = "Jan T. Kim",
                      author_email = "jtk@cmp.uea.ac.uk",
                      packages = ['transsys'],
-		     scripts = ['trsys2sbml'],
+		     scripts = ['trsys2sbml', 'trsystool'],
                      ext_modules = [clib])
